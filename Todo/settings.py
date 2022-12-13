bf5acd92ab0dcaebaadc8e9ed9bb4dfd77c49b54
Suraj Prasad <surajjprasad@gmail.com>
2022-12-13 14:12:33 +0530

    first deploy

--- Todo/settings.py
+++ Todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z!x8mlz)(v3tq9mcw+!hq+e22a2m=cb(j$79x6q)r!40ot%j38
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -123,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
